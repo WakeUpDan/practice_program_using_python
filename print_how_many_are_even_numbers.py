d6bbde79a975164be0e1dbd2a76fe5a209f9dacc
Dan <dllaneza552@gmail.com>
2026-03-12 15:51:05 +0800

    Initial Commit

--- print_how_many_are_even_numbers.py
+++ print_how_many_are_even_numbers.py
@@ -5,3 +5,10 @@ even_count = 0
 for i in range(10):
 
     number = int(input("Enter number " + str(i+1) + ": "))
+
+    # Check if the number is even
+    if number % 2 == 0:
+        even_count = even_count + 1
+
+# Print total even numbers
+print("Total even numbers entered:", even_count)

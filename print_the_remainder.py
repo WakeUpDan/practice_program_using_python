6112a229e6cf2fe01a30b13edb4e4862204bc8e1
Dan <dllaneza552@gmail.com>
2026-03-12 15:42:24 +0800

    New Commited Code

--- print_the_remainder.py
+++ print_the_remainder.py
@@ -1,3 +1,9 @@
 # Input two numbers from the user
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# Check if divisor is zero
+if num2 != 0:
+
+    # Calculate the remainder
+    remainder = num1 % num2

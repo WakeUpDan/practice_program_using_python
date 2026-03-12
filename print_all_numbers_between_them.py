fab8c32439eb19b9fd3072aebbd9be3299d49b53
Dan <dllaneza552@gmail.com>
2026-03-12 15:59:51 +0800

    Added the last commit

--- print_all_numbers_between_them.py
+++ print_all_numbers_between_them.py
@@ -9,3 +9,9 @@ if num1 < num2:
 else:
     start = num2
     end = num1
+
+# Print numbers between them
+print("Numbers between the two values are:")
+
+for i in range(start + 1, end):
+    print(i)

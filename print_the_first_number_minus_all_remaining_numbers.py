9afd0f477c6817801e89141d33f979bdeffe1cb0
Dan <dllaneza552@gmail.com>
2026-03-12 15:47:09 +0800

    Added a new commit

--- print_the_first_number_minus_all_remaining_numbers.py
+++ print_the_first_number_minus_all_remaining_numbers.py
@@ -5,3 +5,10 @@ numbers = []
 for i in range(10):
     num = float(input("Enter number " + str(i+1) + ": "))
     numbers.append(num)
+
+    # Start with the first number
+result = numbers[0]
+
+# Subtract the remaining numbers
+for i in range(1, 10):
+    result = result - numbers[i]

758d6f9ac48ced974f898a7abb1a3eac6c375f67
Dan <dllaneza552@gmail.com>
2026-03-12 15:57:13 +0800

    Added the last code

--- print_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
+++ print_numbers_from_0_to_100_except_numbers_ending_in_0_or_5.py
@@ -3,3 +3,9 @@ for number in range(0, 101):
 
     # Get the last digit of the number
     last_digit = number % 10
+
+    # Check if the number does NOT end with 0 or 5
+    if last_digit != 0 and last_digit != 5:
+
+        # Print the number
+        print(number)

a71b21d798acb7f5e95bd475a1310fe1590b7489
Dan <dllaneza552@gmail.com>
2026-03-12 15:59:00 +0800

    Added the second commit

--- print_all_numbers_between_them.py
+++ print_all_numbers_between_them.py
@@ -1,3 +1,11 @@
 # Ask the user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# Determine the smaller and larger number
+if num1 < num2:
+    start = num1
+    end = num2
+else:
+    start = num2
+    end = num1

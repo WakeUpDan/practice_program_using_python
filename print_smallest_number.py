8f23f1a31a7c08cdcdbed93047ffc1b4985eeada
Dan <dllaneza552@gmail.com>
2026-03-12 13:07:09 +0800

    Added the remaining code

--- print_smallest_number.py
+++ print_smallest_number.py
@@ -3,3 +3,7 @@ num2 = float(input("Enter second number: "))
 
 if num1 < num2:
     print("Smaller number is:", num1)
+elif num2 < num1:
+    print("Smaller number is:", num2)
+else:
+    print("Both numbers are equal")

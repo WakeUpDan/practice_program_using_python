aa7a4ef971fe745b6d0612c5897a93d76006da60
Dan <dllaneza552@gmail.com>
2026-03-12 15:54:23 +0800

    Added a new commit

--- print_all_odd_numbers_from_0_to_100_using_while_loop.py
+++ print_all_odd_numbers_from_0_to_100_using_while_loop.py
@@ -3,3 +3,10 @@ num = 0
 
 # Continue loop until number reaches 100
 while num <= 100:
+
+    # Check if the number is odd
+    if num % 2 != 0:
+        print(num)
+
+    # Increase the number by 1 each loop
+    num = num + 1

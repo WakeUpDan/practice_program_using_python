ce275fab5a7900d88928a82e30ce03590cd1310a
Dan <dllaneza552@gmail.com>
2026-03-12 15:36:36 +0800

    New Commit

--- print_the_quotient_without_the_decimal_point.py
+++ print_the_quotient_without_the_decimal_point.py
@@ -3,3 +3,9 @@ num1 = int(input("Enter the first number: "))
 
 # Ask the user to enter the second number
 num2 = int(input("Enter the second number: "))
+
+# Check if the second number is not zero
+if num2 != 0:
+
+    # Calculate the quotient using floor division
+    quotient = num1 // num2

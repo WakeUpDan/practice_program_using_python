9cb0d0d70d9d3e0dfe77a956178589df3ecfaa94
Dan <dllaneza552@gmail.com>
2026-03-19 23:19:56 +0800

    Updated

--- print_the_quotient_of_the_two_numbers_with_the_decimal_point.py
+++ print_the_quotient_of_the_two_numbers_with_the_decimal_point.py
@@ -1,2 +1,8 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+if num2 != 0:
+    quotient = num1 / num2
+    print("Quotient is:", quotient)
+else:
+    print("Cannot divide by zero")
